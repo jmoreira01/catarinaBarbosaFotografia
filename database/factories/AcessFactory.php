45442118688c2c37745cb1a4ec94250dba50f1a3
jmoreira01 <102956207+jmoreira01@users.noreply.github.com>
2022-11-04 11:36:27 +0000

    updateDB

---
+++ database/factories/AcessFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Acess;
+use Faker\Generator as Faker;
+
+$factory->define(Acess::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/dashboard', 'HomeController@index')->name('dashboard');
