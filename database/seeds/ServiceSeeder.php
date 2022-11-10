ebe4fa96ad858b2cf74e9c82daed356f972d72f7
João <joaopedrosampaioaraujo@gmail.com>
2022-11-10 11:54:46 +0000

    Crud de Imagens e serviços adicionados v3

--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -15,34 +15,21 @@ class ServiceSeeder extends Seeder
             'name' => 'Sessões Grávida',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit auctor dolor id congue. Vivamus convallis purus dolor, vel gravida felis suscipit sit amet. Nullam efficitur dolor quis orci fringilla, ac varius odio vestibulum. Aliquam eget cursus ante. Vivamus ullamcorper vestibulum risus, quis scelerisque quam dictum ut. Vivamus interdum nunc vestibulum erat cursus, nec convallis purus ullamcorper. Duis tincidunt mi nisl, sed dictum ligula finibus lacinia. Proin ac lacus ornare, laoreet ante non, euismod magna. Curabitur ut eros eu mauris varius venenatis ut eget velit. Vivamus enim lectus, pharetra id arcu non, lacinia viverra leo.'
         ]);
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
         DB::table('services')->insert([
             'name' => 'Newborn',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit auctor dolor id congue. Vivamus convallis purus dolor, vel gravida felis suscipit sit amet. Nullam efficitur dolor quis orci fringilla, ac varius odio vestibulum. Aliquam eget cursus ante. Vivamus ullamcorper vestibulum risus, quis scelerisque quam dictum ut. Vivamus interdum nunc vestibulum erat cursus, nec convallis purus ullamcorper. Duis tincidunt mi nisl, sed dictum ligula finibus lacinia. Proin ac lacus ornare, laoreet ante non, euismod magna. Curabitur ut eros eu mauris varius venenatis ut eget velit. Vivamus enim lectus, pharetra id arcu non, lacinia viverra leo.'
         ]);
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
         DB::table('services')->insert([
             'name' => 'Crianças',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit auctor dolor id congue. Vivamus convallis purus dolor, vel gravida felis suscipit sit amet. Nullam efficitur dolor quis orci fringilla, ac varius odio vestibulum. Aliquam eget cursus ante. Vivamus ullamcorper vestibulum risus, quis scelerisque quam dictum ut. Vivamus interdum nunc vestibulum erat cursus, nec convallis purus ullamcorper. Duis tincidunt mi nisl, sed dictum ligula finibus lacinia. Proin ac lacus ornare, laoreet ante non, euismod magna. Curabitur ut eros eu mauris varius venenatis ut eget velit. Vivamus enim lectus, pharetra id arcu non, lacinia viverra leo.'
         ]);
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
         DB::table('services')->insert([
             'name' => 'Família',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit auctor dolor id congue. Vivamus convallis purus dolor, vel gravida felis suscipit sit amet. Nullam efficitur dolor quis orci fringilla, ac varius odio vestibulum. Aliquam eget cursus ante. Vivamus ullamcorper vestibulum risus, quis scelerisque quam dictum ut. Vivamus interdum nunc vestibulum erat cursus, nec convallis purus ullamcorper. Duis tincidunt mi nisl, sed dictum ligula finibus lacinia. Proin ac lacus ornare, laoreet ante non, euismod magna. Curabitur ut eros eu mauris varius venenatis ut eget velit. Vivamus enim lectus, pharetra id arcu non, lacinia viverra leo.'
         ]);
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
         DB::table('services')->insert([
             'name' => 'Eventos',
             'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin hendrerit auctor dolor id congue. Vivamus convallis purus dolor, vel gravida felis suscipit sit amet. Nullam efficitur dolor quis orci fringilla, ac varius odio vestibulum. Aliquam eget cursus ante. Vivamus ullamcorper vestibulum risus, quis scelerisque quam dictum ut. Vivamus interdum nunc vestibulum erat cursus, nec convallis purus ullamcorper. Duis tincidunt mi nisl, sed dictum ligula finibus lacinia. Proin ac lacus ornare, laoreet ante non, euismod magna. Curabitur ut eros eu mauris varius venenatis ut eget velit. Vivamus enim lectus, pharetra id arcu non, lacinia viverra leo.'
